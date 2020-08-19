d6d8ce93c22f135e4ae8d5712be12386c55a5eb0
Robert Walker <rwalker6@buffalo.edu>
2020-08-19 17:13:12 -0500

    oops

--- libsrc/pylith/fekernels/IsotropicLinearPoroelasticity.cc
+++ libsrc/pylith/fekernels/IsotropicLinearPoroelasticity.cc
@@ -293,7 +293,7 @@ pylith::fekernels::IsotropicLinearPoroelasticity::g1p_NoGrav(const PylithInt dim
     const PylithScalar fluidViscosity = a[aOff[i_fluidViscosity]];
 
     for (PylithInt d = 0; d < dim; ++d) {
-        g1p[d] -= (isotropicPerm / fluidViscosity) * pressure_x[d];
+        g1p[d] += (isotropicPerm / fluidViscosity) * pressure_x[d];
     } // for
 } // g1p_NoGrav
 
