300159e8ac7674b66ebe709e8a03b0982f01d19b
Robert Walker <langleyreview@gmail.com>
2021-10-01 14:15:32 -0500

    Added test multivariable fault formulation

--- libsrc/pylith/faults/faultsfwd.hh
+++ libsrc/pylith/faults/faultsfwd.hh
@@ -27,11 +27,14 @@
 #if !defined(pylith_faults_faultsfwd_hh)
 #define pylith_faults_faultsfwd_hh
 
-namespace pylith {
-    namespace faults {
+namespace pylith
+{
+    namespace faults
+    {
         class FaultCohesive;
         class FaultCohesiveKin;
         class FaultCohesiveImpulses;
+        class FaultCohesiveKinPoro;
         class AuxiliaryFactoryKinematic;
 
         class KinSrc;
--- pylith/faults/__init__.py
+++ pylith/faults/__init__.py
@@ -22,6 +22,7 @@ __all__ = [
     "FaultCohesiveKin",
     "FaultCohesiveImpulses",
     "AuxSubfieldsFault",
+    "FaultCohesiveKinPoro",
     "KinSrc",
     "KinSrcConstRate",
     "KinSrcStep",
